84bc1cdd03116e66578d39326b5c8fa53aa05821
Fynn9518 <132841252+Fynn9518@users.noreply.github.com>
2023-09-03 13:07:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
 @bot.command()
+@commands.has_permissions(administrator=True)
 async def issue(ctx, anliegen, prioritaet: int, *, beschreibung):
     global issue_count
     issue_count += 1
@@ -33,7 +34,7 @@ async def issue(ctx, anliegen, prioritaet: int, *, beschreibung):
         return
     
     embed = discord.Embed(
-        description=f'**Name:** {ctx.author.name}\n**Server:** {ctx.guild.name}\n**Channel:** {ctx.channel.name}\n**Server ID:** {ctx.guild.id}\n**Channel ID:** {ctx.channel.id}\n**Datum/Uhrzeit:** {datetime.datetime.now().strftime("%d-%m-%Y %H:%M")}\n\n**Anliegen:** {anliegen}\n**Priorität:** {prioritaet}\n\n**Beschreibung:** {beschreibung}',
+        description=f'**Name:** {ctx.author.name}\n**Server:** {ctx.guild.name}\n**Channel:** {ctx.channel.name}\n\n**Server ID:** {ctx.guild.id}\n**Channel ID:** {ctx.channel.id}\n\n**Datum/Uhrzeit:** {datetime.datetime.now().strftime("%d.%m.%Y %H:%M")}\n\n**Anliegen:** {anliegen}\n**Priorität:** {prioritaet}\n\n**Beschreibung:** {beschreibung}',
         color=2105893
     )
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1132957439189331968/1140574869969436793/1113118860627943435.png")
@@ -50,13 +51,43 @@ async def issue(ctx, anliegen, prioritaet: int, *, beschreibung):
     with open("issue.txt", "a") as file:
         file.write(f"Issue #{issue_count}\n")
         file.write(f"User: {ctx.author.name}\n")
-        file.write(f"Datum/Uhrzeit: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        file.write(f"Datum/Uhrzeit: {datetime.datetime.now().strftime('%d.%m.%Y %H:%M')}\n")
         file.write(f"Server: {ctx.guild.name} (ID: {ctx.guild.id})\n")
         file.write(f"Channel: {ctx.channel.name} (ID: {ctx.channel.id})\n")
         file.write(f"Anliegen: {anliegen}\n")
         file.write(f"Priorität: {prioritaet}\n")
         file.write(f"Beschreibung: {beschreibung}\n")
         file.write("----------\n")
+    
+    # Reaktionen hinzufügen
+    await issue_message.add_reaction('✅')
+    await issue_message.add_reaction('❌')
+    await issue_message.add_reaction('✏')
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if user.bot:
+        return
+    
+    # Überprüfen, ob die Reaktion auf das Anliegen-Embed in dem richtigen Ziel-Channel ist
+    if reaction.message.channel.id != 1140576927837605908:
+        return
+    
+    if reaction.emoji == '✅':
+        # Embed löschen und in den neuen Channel senden
+        new_channel = bot.get_channel(1147845331950764062)
+        await reaction.message.delete()
+        await new_channel.send(embed=reaction.message.embeds[0])
+
+    elif reaction.emoji == '❌':
+        # Embed löschen und in den neuen Channel senden
+        new_channel = bot.get_channel(1147845331950764062)
+        await reaction.message.delete()
+        await new_channel.send(embed=reaction.message.embeds[0])
+
+    elif reaction.emoji == '✏':
+        # Embed in diesem Channel anpinnen
+        await reaction.message.pin()
 
     
 TOKEN = 'BOT_TOKEN'  # Hier den Token deines Bots einfügen
